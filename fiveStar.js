d0d0057b2c9d6d5f00fe81fc6db9e12631235a08
joshuadickerson <joshuajdickerson@gmail.com>
2013-11-03 13:05:23 -0500

    working better now

--- fiveStar.js
+++ fiveStar.js
@@ -11,9 +11,11 @@
         for(var ii=0; ii<5; ii++){
             stars += "<li class='starGroupLi'>";
             stars += "<div class='star star_false'></div>";
-            stars += "<input type='hidden' value='0' class='starValue starValue"+ii+"'/>";
+            stars += "<input type='hidden' value='0' index='"+ii+"' class='starValue starValue"+ii+"'/>";
             stars += "</li>"
         }
+
+        stars += "<li><input type='hidden' class='fiveStarTotal' value='0' /></li></ul>";
 
         this.html(stars);
         $('.starGroup').css("list-style-type", "none");
@@ -23,22 +25,24 @@
         $('.star').css({"background-repeat": "no-repeat"});
 
         $('.starGroupLi').click(function(){
-            if($(this).find('.star').hasClass("star_false")){
-                $(this).find('.star').removeClass("star_false");
-                $(this).find('.star').addClass("star_true");
-                $(this).find('.starValue').val(1);
-                $('.star_true').css({"background":"url('images/star.png')", "width":"40px", "height":"40px"});
-                $('.star_false').css({"background":"url('images/star_empty.png')", "width":"40px", "height":"40px"});
-                $('.star').css({"background-repeat": "no-repeat"});
-
-            }else{
-                $(this).find('.star').addClass("star_false");
-                $(this).find('.star').removeClass("star_true");
-                $(this).find('.starValue').val(0);
-                $('.star_true').css({"background":"url('images/star.png')", "width":"40px", "height":"40px"});
-                $('.star_false').css({"background":"url('images/star_empty.png')", "width":"40px", "height":"40px"});
-                $('.star').css({"background-repeat": "no-repeat"});
+            var starIndex = parseInt($(this).find('.starValue').attr("index"));
+            var starValAr = $(this).parent().find('.starValue');
+            var starAr = $(this).parent().find('.star');
+            $(this).parent().find('.fiveStarTotal').val(starIndex+1);
+            console.log($(this).parent().find('.fiveStarTotal').val());
+            for(var zz=starIndex; zz<5; zz++){
+                starValAr[zz].value = "0";
+                starAr[zz].className = "star_false star";
             }
+            for(var ii=0; ii<=starIndex; ii++){
+                starValAr[ii].value = "1";
+                starAr[ii].className = "star_true star";
+            }
+
+            $('.star_true').css({"background":"url('images/star.png')", "width":"40px", "height":"40px"});
+            $('.star_false').css({"background":"url('images/star_empty.png')", "width":"40px", "height":"40px"});
+            $('.star').css({"background-repeat": "no-repeat"});
+
         });
         return this;
     };
